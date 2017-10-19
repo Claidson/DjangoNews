9487eee370684ec4b6bf7e089175ac41f20ea7b4
aluno <aluno@lgb-115-579601>
2017-10-19 08:22:12 -0200

    forms

--- NoticiasTabajara/views.py
+++ NoticiasTabajara/views.py
@@ -3,10 +3,17 @@ from __future__ import unicode_literals
 from django.http import HttpResponse
 
 from django.shortcuts import render
+import forms
 
 # Create your views here.
 def index(request):
+    	
     	return render(request,'NoticiasTabajara/index.html')
 
 def cad(request):
-    	return render(request,'NoticiasTabajara/CadastrarNoticia.html')
+	form = forms.CadastrarNoticia()
+	if request.method == 'POST':
+		form = forms.CadastrarNoticia(request.POST)
+		if form.is_valid():
+			print("Validation susses!")
+	return render(request,'NoticiasTabajara/CadastrarNoticia.html', {'form':form})
