e5f3a2d8fe94fb087b1cbd57e100a148d1a972b0
Fernando <tra_fernando@hotmail.com>
2017-10-26 09:44:59 -0200

    date

--- NoticiasTabajara/forms.py
+++ NoticiasTabajara/forms.py
@@ -1,11 +1,10 @@
-# -*- coding: utf-8 -*-
-from bootstrap_datepicker.widgets import DatePicker
+# -*- coding: utf-8 -*- 
 from django import forms
 
 class CadastrarNoticia(forms.Form):
     titulo = forms.CharField(widget=forms.TextInput(attrs={'class' : 'form-control'}))
     resumo = forms.CharField(widget=forms.Textarea(attrs={'class' : 'form-control'}))
-    data = forms.DateField(widget=DatePicker(options={"format": "dd/mm/yyyy","autoclose": True}))
+    data = forms.DateField(widget=forms.widgets.DateInput(attrs={'type': 'date'}))
     texto = forms.CharField(widget=forms.Textarea(attrs={'class' : 'form-control'}))
 
     def __str__(self):
